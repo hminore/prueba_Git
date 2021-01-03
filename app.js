91f1c02381ec90893fd256b6b980e70d928b4453
hminore <minorehernan@gmail.com>
2021-01-03 16:57:51 -0300

    prueba console.log

---
+++ app.js
@@ -0,0 +1,11 @@
+// Prueba console.log()
+
+
+
+console.group('group1');
+console.error("Esto es un error");
+console.warn("Esto es un warning");
+console.groupEnd('group1');
+
+console.table({a: 1, b: 2, c: 3});
+
